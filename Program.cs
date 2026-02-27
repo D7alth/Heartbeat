5cadfeece0bdc29f157cf45faa4abb519c8a5fa0
Alberth Henrique <alberthhls@protonmail.com>
2026-02-26 21:08:52 -0300

    add host application setup with dependency injection for metric collection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Heartbeat.Producer.Core.Contracts;
+using Heartbeat.Producer.Infrastructure.Services;
+using Heartbeat.Producer.Infrastructure.Stubs.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = new HostApplicationBuilder();
+builder.Services.AddSingleton<IMetricCollector, MetricCollectorStub>();
+builder.Services.AddHostedService<ServiceWorker>(opt => new ServiceWorker(
+    builder.Services.BuildServiceProvider().GetRequiredService<IMetricCollector>(),
+    builder.Services.BuildServiceProvider().GetRequiredService<ILogger<ServiceWorker>>()
+));
+var app = builder.Build();
+app.Run();
