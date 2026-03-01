c5dbfa97a8e7dae3c9ce32b00e4b1dcd1c22047a
Alberth Henrique <alberthhls@protonmail.com>
2026-03-01 11:51:09 -0300

    update ServiceWorker to include event publishing functionality

--- Infrastructure/Services/ServiceWorker.cs
+++ Infrastructure/Services/ServiceWorker.cs
@@ -1,12 +1,16 @@
 using Heartbeat.Producer.Core.Contracts;
 using Heartbeat.Producer.Core.Helpers.GetMetrics;
+using Heartbeat.Producer.Infrastructure.Services.EventPublish;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Heartbeat.Producer.Infrastructure.Services;
 
-internal sealed class ServiceWorker(IMetricCollector metricCollector, ILogger<ServiceWorker> logger)
-    : BackgroundService
+internal sealed class ServiceWorker(
+    IMetricCollector metricCollector,
+    IPublisher publish,
+    ILogger<ServiceWorker> logger
+) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -23,6 +27,28 @@ internal sealed class ServiceWorker(IMetricCollector metricCollector, ILogger<Se
                 Network Metrics: {eventModel.EventData.NetworkMetrics}"
             );
             await Task.Delay(10_000, stoppingToken);
+            try
+            {
+                var result = await publish.PublishEventAsync(eventModel, stoppingToken);
+                if (result)
+                {
+                    logger.LogInformation(
+                        $"Event with Id: {eventModel.Id} published successfully."
+                    );
+                }
+                else
+                {
+                    logger.LogError($"Failed to publish event with Id: {eventModel.Id}.");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(
+                    ex,
+                    $"An error occurred while publishing event with Id: {eventModel.Id}."
+                );
+                throw;
+            }
         }
     }
 }
