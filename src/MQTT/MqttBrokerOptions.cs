7361d8aaa54b9a130c316defa8cd6af51a189af3
Alberth Henrique <alberthhls@protonmail.com>
2026-06-21 22:09:27 -0300

    feat: add MqttBrokerOptions class for MQTT broker configuration

---
+++ src/MQTT/MqttBrokerOptions.cs
@@ -0,0 +1,12 @@
+namespace src.MQTT;
+
+public sealed class MqttBrokerOptions
+{
+    public required string Broker { get; init; }
+    public required string Topic { get; init; }
+    public required string Username { get; init; }
+    public required string Password { get; init; }
+    public int Port { get; init; }
+    public string ClientId { get; init; } = Guid.CreateVersion7().ToString();
+    public required string TslCertificatePath { get; init; }
+}
