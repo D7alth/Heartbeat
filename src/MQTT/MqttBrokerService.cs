d8de50221f4c66deb890e10daeb0bd024944827d
Alberth Henrique <alberthhls@protonmail.com>
2026-06-23 11:48:32 -0300

    feat: implement MqttBrokerService for handling MQTT message reception

--- src/MQTT/MqttBrokerService.cs
+++ src/MQTT/MqttBrokerService.cs
@@ -1,3 +1,35 @@
+using System.Buffers;
+using System.Text;
+using Microsoft.Extensions.Hosting;
+using src.MQTT.Connection;
+
 namespace src.MQTT;
 
-public class MqttBrokerService { }
+public sealed class MqttBrokerService(IMqttConnectionManager connectionManager) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                var mqttClient = await connectionManager.TryGetConnection(stoppingToken);
+                mqttClient.ApplicationMessageReceivedAsync += e =>
+                {
+                    Console.WriteLine(
+                        $"message recived {ParseBytesToUtf8(e.ApplicationMessage.Payload)}"
+                    );
+                    return Task.CompletedTask;
+                };
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    private static string ParseBytesToUtf8(ReadOnlySequence<byte> byteSpan) =>
+        Encoding.UTF8.GetString(byteSpan);
+}
