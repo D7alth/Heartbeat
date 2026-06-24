f2cbc34ea2008a572385888d4333d674908a5123
Alberth Henrique <alberthhls@protonmail.com>
2026-06-24 07:24:49 -0300

    feat: configure MQTT options and register services in Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,19 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using src.MQTT;
+using src.MQTT.Configuration;
+using src.MQTT.Connection;
 
 var builder = Host.CreateApplicationBuilder();
+
+builder
+    .Services.AddOptions<MqttBrokerOptions>()
+    .Bind(builder.Configuration.GetSection("Mqtt"))
+    .ValidateOnStart();
+
+builder.Services.AddSingleton<IMqttConnectionManager, MqttConnection>();
+builder.Services.AddHostedService<MqttBrokerService>();
+
 var host = builder.Build();
 
 await host.RunAsync();
