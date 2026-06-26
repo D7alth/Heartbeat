308deb35e6910ea8895a808d2410cfee8d7ed990
Alberth Henrique <alberthhls@protonmail.com>
2026-06-26 11:54:41 -0300

    refactor: optimize MQTT client connection handling in MqttBrokerService

--- src/MQTT/MqttBrokerService.cs
+++ src/MQTT/MqttBrokerService.cs
@@ -1,20 +1,24 @@
 using System.Buffers;
 using System.Text;
 using Microsoft.Extensions.Hosting;
+using MQTTnet;
 using src.MQTT.Connection;
 
 namespace src.MQTT;
 
 public sealed class MqttBrokerService(IMqttConnectionManager connectionManager) : BackgroundService
 {
+    private static IMqttClient? _mqttClient;
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
             try
             {
-                var mqttClient = await connectionManager.TryGetConnection(stoppingToken);
-                mqttClient.ApplicationMessageReceivedAsync += e =>
+                _mqttClient ??= await connectionManager.TryGetConnection(stoppingToken);
+                if (_mqttClient is null)
+                    return;
+                _mqttClient.ApplicationMessageReceivedAsync += e =>
                 {
                     Console.WriteLine(
                         $"message recived {ParseBytesToUtf8(e.ApplicationMessage.Payload)}"
